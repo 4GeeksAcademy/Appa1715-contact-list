37556842e8a23138d9780b8f670aa62c67e8739b
Appa1715 <128950756+Appa1715@users.noreply.github.com>
2023-08-06 23:15:27 +0000

    lol

---
+++ src/js/component/card.js
@@ -0,0 +1,28 @@
+
+import React, { useContext } from "react"
+
+const Card = ({ contact }) => {
+	const { store, actions } = useContext(Context);
+  
+	const handleSubmit = () => {
+	  actions.startEditing(contact.id);
+	};
+  
+	const handleDelete = () => {
+	  actions.deleteContact(contact.id);
+	};
+
+	return (
+		<div className="card" style="width: 18rem;">
+  			<div className="card-header">
+    			<h1>CONTACTS</h1>
+				<button>ADD CONTACT</button>
+  			</div>
+			<ul className="list-group list-group-flush">
+    			<li className="list-group-item">An item</li>
+    			<li className="list-group-item">A second item</li>
+    			<li className="list-group-item">A third item</li>
+			</ul>
+		</div>
+	)
+};
