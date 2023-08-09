cad37d2228dbed82cade068143deb6bf94fca4f2
Appa1715 <128950756+Appa1715@users.noreply.github.com>
2023-08-09 10:46:03 +0000

    hope2

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,18 +1,49 @@
-import React, { useContext } from "react";
-import { Context } from "...store/appContext";
+import React from "react";
+import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import { Card } from "...component/Card";
+import { Context } from "../store/appContext";
+import { EditContact } from "../views/single";
 
-export const Home = () => {
-	const { store } = useContext(Context);
-  
+export const Home = ({contact}) => {
+	const { store, actions } = useContext(Context);
+	const addEdidedInfo = () => {
+		actions.submitInfo(contact.id)
+	};
+	const deleteInfo = () => {
+		actions.cancelInfo (contact.id)
+	};
+
+
 	return (
-	  <div className="mt-5 ">
-		{store.contacts.map((contact) => (
-		  <div key={contact.id} className="col-12 mb-4">
-			<Card contact={contact} />
-		  </div>
-		))}
-	  </div>
-	);
-  };
+		<div className="text-center mt-5">
+			<div className="card" >
+				<div className="row">
+					<div className="col">
+						
+					</div>
+					<div className="col-5">
+						<div className="card-body">
+							<h5 className="card-title">{Name}</h5>
+							<p className="card-text"><strong></strong>{address}</p>
+							<p className="card-text"><strong></strong>{phone}</p>
+							<p className="card-text"><strong></strong>{email}</p>
+
+						</div>
+					</div>
+					<div className="col">
+						{!contact.isEditing ? (
+							<>
+								<button type="button" 
+								className="btn btn-success"
+								onClick={addEdidedInfo}>Submit</button>
+								<button type="button" 
+								className="btn btn-success"
+								onClick={deleteInfo}>Cancel</button>
+							</>): (<EditContact contact={contact} />)}
+					</div>
+			</div>
+		</div>
+	</div>
+	)
+	
+};
